5c7aaf50ca3c7a3220a54f4e35957be5dc2d8eb1
ronan03 <ronan03000@gmail.com>
2020-04-01 17:56:35 +0200

    commit homing feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1364,8 +1364,8 @@
 #endif
 
 // Homing speeds (mm/m)
-#define HOMING_FEEDRATE_XY (80*60)
-#define HOMING_FEEDRATE_Z  (12*60)
+#define HOMING_FEEDRATE_XY (60*60)
+#define HOMING_FEEDRATE_Z  (4*60)
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
