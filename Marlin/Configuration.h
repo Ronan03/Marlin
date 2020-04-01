ddbb99fd61c5f6791ffec4d7043a0e5b9c0c8ca2
ronan03 <ronan03000@gmail.com>
2020-04-01 17:38:37 +0200

    commit bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1097,10 +1097,10 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -35
-#define Y_MIN_POS -15
+#define Y_MIN_POS -18
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS 207
+#define Y_MAX_POS 217
 #define Z_MAX_POS 220
 
 /**
